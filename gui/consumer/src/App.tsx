0ab32818eaaa192e0ad561c766bb30862653fd05
caparicio-esd <caparicio@esdmadrid.es>
2024-11-04 15:11:59 +0100

    add docker deployments and build image

--- gui/consumer/src/App.tsx
+++ gui/consumer/src/App.tsx
@@ -9,6 +9,7 @@ import {
   TableRow,
 } from "shared/src/components/ui/table";
 import { useFetchConsumerCallbacks } from "./hooks/useFetchConsumerCallbacks";
+import { EyeIcon, PencilIcon } from "lucide-react";
 
 const App = () => {
   const { data, isError, error } = useFetchConsumerCallbacks();
@@ -24,6 +25,7 @@ const App = () => {
         <TableCaption>Ongoing consumer callbacks</TableCaption>
         <TableHeader>
           <TableRow>
+            <TableHead className="sticky left-0 top-0 bg-white border-r border-gray-400">Actions:</TableHead>
             <TableHead>Callback Id:</TableHead>
             <TableHead>Created at:</TableHead>
             <TableHead>Updated at:</TableHead>
@@ -36,18 +38,30 @@ const App = () => {
           {data &&
             data.map((d: TransferCallbackModel, i: number) => (
               <TableRow key={i}>
-                <TableCell>{d.id}</TableCell>
-                <TableCell>
+                <TableCell className="sticky left-0 top-0 bg-white border-r border-gray-400">
+                  <div className="flex gap-2 items-center">
+                    <PencilIcon width={16} />
+                    <EyeIcon width={16} />
+                  </div>
+                </TableCell>
+                <TableCell className="truncate overflow-hidden w-1/12">
+                  {d.id}
+                </TableCell>
+                <TableCell className="whitespace-nowrap">
                   {dayjs(d.created_at).format("DD/MM/YYYY HH:mm:ss.SSS")}
                 </TableCell>
-                <TableCell>
+                <TableCell className="whitespace-nowrap">
                   {d.updated_at
                     ? dayjs(d.updated_at).format("DD/MM/YYYY HH:mm:ss.SSS")
                     : "-"}
                 </TableCell>
-                <TableCell>{d.provider_pid ? d.provider_pid : "-"}</TableCell>
-                <TableCell>{d.consumer_pid ? d.consumer_pid : "-"}</TableCell>
-                <TableCell>
+                <TableCell className="truncate overflow-hidden w-1/12">
+                  {d.provider_pid ? d.provider_pid : "-"}
+                </TableCell>
+                <TableCell className="truncate overflow-hidden w-1/12">
+                  {d.consumer_pid ? d.consumer_pid : "-"}
+                </TableCell>
+                <TableCell className="truncate overflow-hidden w-1/12">
                   {d.data_address ? d.data_address["dspace:endpoint"] : "-"}
                 </TableCell>
               </TableRow>
