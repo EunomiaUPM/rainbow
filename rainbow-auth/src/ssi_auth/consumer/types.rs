bc2b927793edf06162b2e750b6dbdbe3775b6be6
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2025-03-21 05:05:43 +0100

    Account Implementation

---
+++ rainbow-auth/src/ssi_auth/consumer/types.rs
@@ -0,0 +1,33 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct WalletLoginResponse {
+    pub id: String,
+    pub username: String,
+    pub token: String,
+}
+
+#[derive(Deserialize)]
+pub struct WalletInfo {
+    pub id: String,
+    pub name: String,
+    pub created_on: String,
+    pub added_on: String,
+    pub permission: Permission,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+enum Permission {
+    ADMINISTRATE, // ADD MORE
+}
+
+#[derive(Deserialize)]
+pub struct Jwtclaims {
+    pub sub: String,
+    pub exp: u64,
+    pub iat: u64,
+    pub jti: String,
+    pub iss: String,
+    pub aud: String,
+}
