b283682038c50ed2be28dd93161cce953ee6b448
caparicio-esd <caparicio@esdmadrid.es>
2025-06-04 16:27:54 +0200

    tiny changes after rebase

--- rainbow-contracts/src/provider/core/ds_protocol_rpc/ds_protocol_rpc.rs
+++ rainbow-contracts/src/provider/core/ds_protocol_rpc/ds_protocol_rpc.rs
@@ -363,7 +363,7 @@ where
         }
         // 3. create message
         let contract_offer_message = ContractOfferMessage {
-            provider_pid: cn_process.provider_id.unwrap().parse()?,
+            provider_pid: cn_process.provider_id.parse()?,
             odrl_offer: ContractRequestMessageOfferTypes::OfferMessage(odrl_offer.clone()),
             ..Default::default()
         };
