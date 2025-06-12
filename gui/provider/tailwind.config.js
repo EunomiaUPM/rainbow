7e2c4624b6bd5e122edf086b3a13ee362316ab15
lauralog <laura.rdsz97@gmail.com>
2025-06-12 17:21:53 +0200

    tailwind fonts fix + colors update

--- gui/provider/tailwind.config.js
+++ gui/provider/tailwind.config.js
@@ -1,4 +1,3 @@
-
 import {
   theme as theme_,
   plugins as plugins_,
@@ -9,40 +8,49 @@ import {
 module.exports = {
   content: [
     "index.css",
-  "./src/**/*.{ts,tsx,js,jsx,css,sass,scss}",
-  "./../shared/src/**/*.{ts,tsx,js,jsx,css,sass,scss}",
-  "./../shared/index.css"
+    "./src/**/*.{ts,tsx,js,jsx,css,sass,scss}",
+    "./../shared/src/**/*.{ts,tsx,js,jsx,css,sass,scss}",
+    "./../shared/index.css",
   ],
   theme: {
     extend: {
       fontFamily: {
-        // "title": ['var(--font-arimo)', 'sans-serif'],
-        // "display": ['var(--font-dmSans)', 'sans-serif'],
+        ubuntu: ["Ubuntu", "sans-serif"],
+        source: ["Source Sans Pro", "Source Sans 3", "sans-serif"],
       },
       fontSize: {
-        '2xs': ['0.625rem', { lineHeight: '1.4' }],  /* 10px */
-        'xs': ['0.75rem', { lineHeight: '1.4' }],    /* 12px */
-        'sm': ['0.875rem', { lineHeight: '1.4' }],   /* 14px */
-        'base': ['1rem', { lineHeight: '1.5' }],     /* 16px body */
-        '18': ['1.125rem', { lineHeight: '1.5' }],
-        '20': ['1.25rem', { lineHeight: '1.4' }], 
-        '24': ['1.5rem', { lineHeight: '1.4' }], 
-        '28': ['1.75rem', { lineHeight: '1.4' }],
-        '32': ['2rem', { lineHeight: '1.4' }],   
-        '36': ['2.25rem', { lineHeight: '1.4' }],
-        '40': ['2.5rem', { lineHeight: '1.4' }], 
-        '48': ['3rem', { lineHeight: '1.4' }], 
-        '56': ['3.5rem', { lineHeight: '1.4' }], 
-        '64': ['4rem', { lineHeight: '1.4' }],
+        "2xs": ["0.625rem", { lineHeight: "1.4" }] /* 10px */,
+        xs: ["0.75rem", { lineHeight: "1.4" }] /* 12px */,
+        sm: ["0.875rem", { lineHeight: "1.4" }] /* 14px */,
+        base: ["1rem", { lineHeight: "1.5" }] /* 16px body */,
+        18: ["1.125rem", { lineHeight: "1.5" }],
+        20: ["1.25rem", { lineHeight: "1.4" }],
+        24: ["1.5rem", { lineHeight: "1.4" }],
+        28: ["1.75rem", { lineHeight: "1.4" }],
+        32: ["2rem", { lineHeight: "1.4" }],
+        36: ["2.25rem", { lineHeight: "1.4" }],
+        40: ["2.5rem", { lineHeight: "1.4" }],
+        48: ["3rem", { lineHeight: "1.4" }],
+        56: ["3.5rem", { lineHeight: "1.4" }],
+        64: ["4rem", { lineHeight: "1.4" }],
       },
       colors: {
-        // whiteFull: "#ffffff",
-        text: "#f8f8f8",
-        snow: "#EFF7FB",
-        black: "#0D0D1C",
         foreground: "#ffffff",
         text: "#fefefe",
-        primary: { // azul genérico
+        stroke: '#525880',
+        brand: {
+          snow: "#EFF7FB", // white
+          sky: "#9DD5F2", // light blue
+          purple: "#62388E",
+          blue: "#24234C", // dark blue
+          black: "#0D0D1C",
+        },
+        base: {
+          main: "#191934",
+          sidebar: "#1D1C3D",
+          header: "#24234C",
+        },
+        primary: {
           DEFAULT: "#056dc1",
           900: "#121212",
           800: "#121212",
@@ -55,9 +63,8 @@ module.exports = {
           100: "#121212",
           50: "#121212",
           foreground: "#FFFFFF",
-         
         },
-        secondary: { 
+        secondary: {
           DEFAULT: "#121212",
           700: "#121212",
           600: "#121212",
@@ -67,45 +74,43 @@ module.exports = {
           200: "#121212",
           100: "#121212",
           50: "#121212",
-          foreground: "#ffffff"
+          foreground: "#ffffff",
         },
-        accent: { 
-          DEFAULT: "#121212",
+        accent: {
+          DEFAULT: "#2D3B98", // azul, igual termina siendo el primary
         },
-        border: { 
+        border: {
           DEFAULT: "#121212",
         },
         background: {
           600: "#2E3356",
-          DEFAULT:"#2E3356",
-          400:"#2E3356",
-          300: "#2E3356", 
-          200:  "#2E3356"
-      
+          DEFAULT: "#2E3356",
+          400: "#2E3356",
+          300: "#2E3356",
+          200: "#2E3356",
         },
         ring: {
           DEFAULT: "#cbe0ed",
         },
         shadow: {
-          DEFAULT:"#cbd5e1",
+          DEFAULT: "#cbd5e1",
         },
         title: {
-          DEFAULT:"#323232"
+          DEFAULT: "#323232",
         },
       },
     },
     screens: {
-      'xs': '420px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px', 
-      '3xl': '1700px'   
+      xs: "420px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1700px",
     },
-   
-  }, 
-}
+  },
+};
 // export const content = [
 //   "index.css",
 //   "./src/**/*.{ts,tsx,js,jsx,css,sass,scss}",
