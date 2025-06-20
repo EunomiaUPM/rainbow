de1daca425ce84462aa52765ee1ce4ac1274f172
Pablo <pvinualese@gmail.com>
2025-06-20 15:40:43 +0200

    get dataset arreglado

--- rainbow-datahub-catalog/src/core/datahub_proxy/datahub_proxy.rs
+++ rainbow-datahub-catalog/src/core/datahub_proxy/datahub_proxy.rs
@@ -18,8 +18,9 @@
  */
 
 use crate::core::datahub_proxy::datahub_proxy_types::DatasetGraphQLResponseDetailed;
-use crate::core::datahub_proxy::datahub_proxy_types::{
-    DatahubDataset, DatasetBasicInfo, DatasetGraphQLResponse, DomainProperties, GlossaryTerm, TagProperties};
+// use crate::core::datahub_proxy::datahub_proxy_types::{
+//     DatahubDataset, DatasetBasicInfo, DatasetGraphQLResponse, DomainProperties, GlossaryTerm, TagProperties};
+use crate::core::datahub_proxy::datahub_proxy_types::{DatahubDataset, DatasetBasicInfo, DatasetGraphQLResponse, DomainProperties, GlossaryTerm, TagProperties};
 use crate::core::datahub_proxy::datahub_proxy_types::{DatahubDomain, GraphQLResponse, Platform, Tag};
 use crate::core::datahub_proxy::DatahubProxyTrait;
 use crate::setup::config::DatahubCatalogApplicationProviderConfig;
@@ -28,6 +29,7 @@ use rainbow_common::config::provider_config::ApplicationProviderConfigTrait;
 use reqwest::Client;
 use std::time::Duration;
 use tracing::debug;
+
 
 pub struct DatahubProxyService {
     config: DatahubCatalogApplicationProviderConfig,
@@ -322,123 +324,378 @@ impl DatahubProxyTrait for DatahubProxyService {
 
         Ok(datasets)
     }
+
     
-    async fn get_datahub_dataset_by_id(&self, id: String) -> anyhow::Result<DatahubDataset> {
-        let datahub_host = self.config.get_datahub_host_url().expect("Datahub host not created");
-        let datahub_token = self.config.get_datahub_token().expect("Datahub Token not created");
-        let graphql_url = format!("{}/api/graphql", datahub_host);
-        let query = format!(
-            r#"{{
-            dataset(urn: "{}") {{
+    
+    // async fn get_datahub_dataset_by_id(&self, id: String) -> anyhow::Result<DatahubDataset> {
+    //     let datahub_host = self.config.get_datahub_host_url().expect("Datahub host not created");
+    //     let datahub_token = self.config.get_datahub_token().expect("Datahub Token not created");
+    //     let graphql_url = format!("{}/api/graphql", datahub_host);
+    //     let query = format!(
+    //         r#"{{
+    //         dataset(urn: "{}") {{
+    //             urn
+    //             name
+    //             platform {{ name }}
+    //             description
+    //             properties {{
+    //                 name
+    //                 description
+    //                 customProperties {{
+    //                     key
+    //                     value
+    //                 }}
+    //             }}
+    //             ownership {{
+    //                 owners {{
+    //                     owner {{
+    //                         ... on CorpUser {{
+    //                             username
+    //                         }}
+    //                     }}
+    //                 }}
+    //             }}
+    //             tags {{
+    //                 tags {{
+    //                     tag {{
+    //                         name
+    //                     }}
+    //                 }}
+    //             }}
+    //             schemaMetadata {{
+    //                 fields {{
+    //                     fieldPath
+    //                     type
+    //                 }}
+    //             }}
+    //             domain {{
+    //                 associatedUrn
+    //                 domain {{
+    //                     urn
+    //                     properties {{
+    //                         name
+    //                     }}
+    //                 }}
+    //             }}
+    //             glossaryTerms {{
+    //                 terms {{
+    //                     term {{
+    //                         urn
+    //                         glossaryTermInfo {{
+    //                             name
+    //                             description
+    //                         }}
+    //                     }}
+    //                 }}
+    //             }}
+    //         }}
+    //     }}"#,
+    //         id
+    //     );
+
+    //     // let request_body = serde_json::json!({
+    //     //     "query": query
+    //     // });
+
+    //     // let response = self
+    //     //     .client
+    //     //     .post(graphql_url)
+    //     //     .header("Content-Type", "application/json")
+    //     //     .header("Authorization", format!("Bearer {}", datahub_token))
+    //     //     .json(&request_body)
+    //     //     .send()
+    //     //     .await?;
+
+    //     // let graphql_response: DatasetGraphQLResponseDetailed = response.json().await?;
+
+    //     // let dataset = graphql_response.data.dataset;
+
+    //     // let custom_props: Vec<(String, String)> = dataset.properties.customProperties
+    //     //     .unwrap_or_default()
+    //     //     .into_iter()
+    //     //     .map(|cp| (cp.key, cp.value))
+    //     //     .collect();
+
+    //     // let domain = DatahubDomain {
+    //     //     urn: dataset.domain.domain.urn,
+    //     //     properties: DomainProperties {
+    //     //         name: dataset.domain.domain.properties.name,
+    //     //         description: None,
+    //     //     },
+    //     // };
+
+    //     // let glossary_terms = dataset.glossaryTerms.map(|gt| {
+    //     //     gt.terms
+    //     //         .into_iter()
+    //     //         .map(|t| t.term)
+    //     //         .collect::<Vec<GlossaryTerm>>()
+    //     // });
+
+    //     // let dataset = DatahubDataset {
+    //     //     urn: dataset.urn,
+    //     //     name: dataset.name,
+    //     //     platform: dataset.platform,
+    //     //     description: dataset.description,
+    //     //     tag_names: dataset.tags.tags.into_iter().map(|tw| tw.tag.properties.name).collect(),
+    //     //     custom_properties: custom_props,
+    //     //     domain,
+    //     //     glossary_terms,
+    //     // };
+
+    //     let client = reqwest::Client::new();
+    //     let res = client
+    //         .post(&graphql_url)
+    //         .bearer_auth(datahub_token)
+    //         .json(&serde_json::json!({ "query": query }))
+    //         .send()
+    //         .await?
+    //         .json::<serde_json::Value>()
+    //         .await?;
+
+    //     if let Some(results) = res["data"]["searchAcrossEntities"]["searchResults"].as_array() {
+    //         // for result in results {
+    //             // if let Some(entity) = result.get("entity") {
+    //                 let urn = entity.get("urn").and_then(|v| v.as_str()).unwrap_or_default().to_string();
+    //                 let name = entity.get("name").and_then(|v| v.as_str()).unwrap_or_default().to_string();
+    //                 let platform = entity.get("platform")
+    //                     .cloned()
+    //                     .and_then(|p| serde_json::from_value(p).ok())
+    //                     .unwrap_or_else(|| Platform { name: "".to_string() });
+    //                 let description = entity.get("description").and_then(|v| v.as_str()).map(|s| s.to_string());
+
+    //                 let tag_names = entity
+    //                     .get("tags")
+    //                     .and_then(|tags| tags.get("tags"))
+    //                     .and_then(|tags| tags.as_array())
+    //                     .map(|tags| {
+    //                         tags.iter()
+    //                             .filter_map(|tw| tw.get("tag").and_then(|tag| tag.get("name")).and_then(|n| n.as_str()).map(|s| s.to_string()))
+    //                             .collect::<Vec<String>>()
+    //                     })
+    //                     .unwrap_or_default();
+
+    //                 let custom_props: Vec<(String, String)> = entity
+    //                     .get("properties")
+    //                     .and_then(|p| p.get("customProperties"))
+    //                     .and_then(|cp| cp.as_array())
+    //                     .map(|arr| {
+    //                         arr.iter()
+    //                             .filter_map(|cp| {
+    //                                 let key = cp.get("key")?.as_str()?;
+    //                                 let value = cp.get("value")?.as_str()?;
+    //                                 Some((key.to_string(), value.to_string()))
+    //                             })
+    //                             .collect()
+    //                     })
+    //                     .unwrap_or_default();
+
+    //                 let domain = entity
+    //                     .get("domain")
+    //                     .and_then(|d| d.get("domain"))
+    //                     .map(|d| DatahubDomain {
+    //                         urn: d.get("urn").and_then(|v| v.as_str()).unwrap_or_default().to_string(),
+    //                         properties: DomainProperties {
+    //                             name: d.get("properties").and_then(|p| p.get("name")).and_then(|n| n.as_str()).unwrap_or_default().to_string(),
+    //                             description: None,
+    //                         },
+    //                     })
+    //                     .unwrap_or(DatahubDomain {
+    //                         urn: "".to_string(),
+    //                         properties: DomainProperties {
+    //                             name: "".to_string(),
+    //                             description: None,
+    //                         },
+    //                     });
+
+    //                 let glossary_terms = entity
+    //                     .get("glossaryTerms")
+    //                     .and_then(|gt| gt.get("terms"))
+    //                     .and_then(|terms| terms.as_array())
+    //                     .map(|terms| {
+    //                         terms
+    //                             .iter()
+    //                             .filter_map(|t| t.get("term"))
+    //                             .filter_map(|term| serde_json::from_value::<GlossaryTerm>(term.clone()).ok())
+    //                             .collect::<Vec<GlossaryTerm>>()
+    //                     });
+
+    //                 let dataset = DatahubDataset {
+    //                     urn,
+    //                     name,
+    //                     platform,
+    //                     description,
+    //                     tag_names,
+    //                     custom_properties: custom_props,
+    //                     domain,
+    //                     glossary_terms,
+    //                 };
+    //                 return Ok(dataset);
+                    
+                
+            
+    //     }
+        
+    //     // Ok(dataset)
+    // }
+
+    async fn get_datahub_dataset_by_id(&self, urn: String) -> anyhow::Result<DatahubDataset> {
+    let datahub_host = self.config.get_datahub_host_url().expect("Datahub host not created");
+    let datahub_token = self.config.get_datahub_token().expect("Datahub Token not created");
+    let graphql_url = format!("{}/api/graphql", datahub_host);
+
+    let query = format!(
+        r#"{{
+            entity(urn: "{}") {{
                 urn
-                name
-                platform {{ name }}
-                description
-                properties {{
+                ... on Dataset {{
                     name
+                    platform {{ name }}
                     description
-                    customProperties {{
-                        key
-                        value
+                    properties {{
+                        name
+                        description
+                        customProperties {{
+                            key
+                            value
+                        }}
                     }}
-                }}
-                ownership {{
-                    owners {{
-                        owner {{
-                            ... on CorpUser {{
-                                username
+                    ownership {{
+                        owners {{
+                            owner {{
+                                ... on CorpUser {{
+                                    username
+                                }}
                             }}
                         }}
                     }}
-                }}
-                tags {{
                     tags {{
-                        tag {{
-                            name
-                        }}
-                    }}
-                }}
-                schemaMetadata {{
-                    fields {{
-                        fieldPath
-                        type
-                    }}
-                }}
-                domain {{
-                    associatedUrn
-                    domain {{
-                        urn
-                        properties {{
-                            name
-                        }}
-                    }}
-                }}
-                glossaryTerms {{
-                    terms {{
-                        term {{
-                            urn
-                            glossaryTermInfo {{
+                        tags {{
+                            tag {{
                                 name
-                                description
+                            }}
+                        }}
+                    }}
+                    schemaMetadata {{
+                        fields {{
+                            fieldPath
+                            type
+                        }}
+                    }}
+                    domain {{
+                        associatedUrn
+                        domain {{
+                            urn
+                            properties {{
+                                name
+                            }}
+                        }}
+                    }}
+                    glossaryTerms {{
+                        terms {{
+                            term {{
+                                urn
+                                glossaryTermInfo {{
+                                    name
+                                    description
+                                }}
                             }}
                         }}
                     }}
                 }}
             }}
         }}"#,
-            id
-        );
+        urn
+    );
 
-        let request_body = serde_json::json!({
-            "query": query
-        });
+    let client = reqwest::Client::new();
+    let res = client
+        .post(&graphql_url)
+        .bearer_auth(datahub_token)
+        .json(&serde_json::json!({ "query": query }))
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
 
-        let response = self
-            .client
-            .post(graphql_url)
-            .header("Content-Type", "application/json")
-            .header("Authorization", format!("Bearer {}", datahub_token))
-            .json(&request_body)
-            .send()
-            .await?;
+    let entity = res["data"]["entity"]
+        .as_object()
+        .ok_or_else(|| anyhow::anyhow!("Dataset with URN '{}' not found", urn))?;
 
-        let graphql_response: DatasetGraphQLResponseDetailed = response.json().await?;
+    let urn = entity.get("urn").and_then(|v| v.as_str()).unwrap_or_default().to_string();
+    let name = entity.get("name").and_then(|v| v.as_str()).unwrap_or_default().to_string();
+    let platform = entity.get("platform")
+        .cloned()
+        .and_then(|p| serde_json::from_value(p).ok())
+        .unwrap_or_else(|| Platform { name: "".to_string() });
+    let description = entity.get("description").and_then(|v| v.as_str()).map(|s| s.to_string());
 
-        let dataset = graphql_response.data.dataset;
+    let tag_names = entity
+        .get("tags")
+        .and_then(|tags| tags.get("tags"))
+        .and_then(|tags| tags.as_array())
+        .map(|tags| {
+            tags.iter()
+                .filter_map(|tw| tw.get("tag").and_then(|tag| tag.get("name")).and_then(|n| n.as_str()).map(|s| s.to_string()))
+                .collect::<Vec<String>>()
+        })
+        .unwrap_or_default();
 
-        let custom_props: Vec<(String, String)> = dataset.properties.customProperties
-            .unwrap_or_default()
-            .into_iter()
-            .map(|cp| (cp.key, cp.value))
-            .collect();
+    let custom_props: Vec<(String, String)> = entity
+        .get("properties")
+        .and_then(|p| p.get("customProperties"))
+        .and_then(|cp| cp.as_array())
+        .map(|arr| {
+            arr.iter()
+                .filter_map(|cp| {
+                    let key = cp.get("key")?.as_str()?;
+                    let value = cp.get("value")?.as_str()?;
+                    Some((key.to_string(), value.to_string()))
+                })
+                .collect()
+        })
+        .unwrap_or_default();
 
-        let domain = DatahubDomain {
-            urn: dataset.domain.domain.urn,
+    let domain = entity
+        .get("domain")
+        .and_then(|d| d.get("domain"))
+        .map(|d| DatahubDomain {
+            urn: d.get("urn").and_then(|v| v.as_str()).unwrap_or_default().to_string(),
             properties: DomainProperties {
-                name: dataset.domain.domain.properties.name,
+                name: d.get("properties").and_then(|p| p.get("name")).and_then(|n| n.as_str()).unwrap_or_default().to_string(),
                 description: None,
             },
-        };
+        })
+        .unwrap_or(DatahubDomain {
+            urn: "".to_string(),
+            properties: DomainProperties {
+                name: "".to_string(),
+                description: None,
+            },
+        });
 
-        let glossary_terms = dataset.glossaryTerms.map(|gt| {
-            gt.terms
-                .into_iter()
-                .map(|t| t.term)
+    let glossary_terms = entity
+        .get("glossaryTerms")
+        .and_then(|gt| gt.get("terms"))
+        .and_then(|terms| terms.as_array())
+        .map(|terms| {
+            terms
+                .iter()
+                .filter_map(|t| t.get("term"))
+                .filter_map(|term| serde_json::from_value::<GlossaryTerm>(term.clone()).ok())
                 .collect::<Vec<GlossaryTerm>>()
         });
 
-        let dataset = DatahubDataset {
-            urn: dataset.urn,
-            name: dataset.name,
-            platform: dataset.platform,
-            description: dataset.description,
-            tag_names: dataset.tags.tags.into_iter().map(|tw| tw.tag.properties.name).collect(),
-            custom_properties: custom_props,
-            domain,
-            glossary_terms,
-        };
+    let dataset = DatahubDataset {
+        urn,
+        name,
+        platform,
+        description,
+        tag_names,
+        custom_properties: custom_props,
+        domain,
+        glossary_terms,
+    };
 
-        
-        Ok(dataset)
-    }
+    Ok(dataset)
+}
 }
