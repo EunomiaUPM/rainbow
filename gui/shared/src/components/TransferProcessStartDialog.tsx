fe820c6a289311a8365a3d246e9cc21e362bd410
caparicio-esd <caparicio@esdmadrid.es>
2025-07-07 13:04:19 +0200

    transfer dialogs complete

--- gui/shared/src/components/TransferProcessStartDialog.tsx
+++ gui/shared/src/components/TransferProcessStartDialog.tsx
@@ -1,124 +1,117 @@
-import { GlobalInfoContext, GlobalInfoContextType } from "./../context/GlobalInfoContext";
-import { usePostTransferRPCStart } from "shared/src/data/transfer-mutations";
+import {GlobalInfoContext, GlobalInfoContextType} from "./../context/GlobalInfoContext";
+import {usePostTransferRPCStart} from "shared/src/data/transfer-mutations";
 import dayjs from "dayjs";
-import {
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "./ui/dialog";
-import { Button } from "./ui/button";
-import React, { useContext } from "react";
-import { Form } from "./ui/form";
-import { useForm } from "react-hook-form";
-import { Badge } from "shared/src/components/ui/badge";
-import { List, ListItem, ListItemKey } from "./ui/list";
+import {DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle,} from "./ui/dialog";
+import {Button} from "./ui/button";
+import React, {useContext} from "react";
+import {Form} from "./ui/form";
+import {useForm} from "react-hook-form";
+import {Badge} from "shared/src/components/ui/badge";
+import {List, ListItem, ListItemKey} from "./ui/list";
 
-export const TransferProcessStartDialog = ({ process }: { process: TransferProcess }) => {
-  // --- Form Setup ---
-  const form = useForm({});
-  const { handleSubmit, control, setValue, getValues } = form;
-  const { mutateAsync: startAsync } = usePostTransferRPCStart();
-  const { api_gateway, role } = useContext<GlobalInfoContextType>(GlobalInfoContext);
-  const onSubmit = () => {
-    if (role == "provider") {
-      return startAsync({
-        api_gateway: api_gateway,
-        content: {
-          consumerParticipantId: process.associated_consumer,
-          consumerCallbackAddress: process.data_plane_id,
-          consumerPid: process.consumer_pid,
-          providerPid: process.provider_pid,
-        },
-      });
-    }
-    if (role == "consumer") {
-      return startAsync({
-        api_gateway: api_gateway,
-        content: {
-          providerParticipantId: process.associated_provider,
-          consumerPid: process.consumer_pid,
-          providerPid: process.provider_pid,
-        },
-      });
-    }
-  };
+export const TransferProcessStartDialog = ({process}: { process: TransferProcess }) => {
+    // --- Form Setup ---
+    const form = useForm({});
+    const {handleSubmit, control, setValue, getValues} = form;
+    const {mutateAsync: startAsync} = usePostTransferRPCStart();
+    const {api_gateway, role} = useContext<GlobalInfoContextType>(GlobalInfoContext);
+    const onSubmit = () => {
+        if (role == "provider") {
+            return startAsync({
+                api_gateway: api_gateway,
+                content: {
+                    consumerParticipantId: process.associated_consumer,
+                    consumerCallbackAddress: process.data_plane_id,
+                    consumerPid: process.consumer_pid,
+                    providerPid: process.provider_pid,
+                },
+            });
+        }
+        if (role == "consumer") {
+            return startAsync({
+                api_gateway: api_gateway,
+                content: {
+                    providerParticipantId: process.associated_provider,
+                    consumerPid: process.consumer_pid,
+                    providerPid: process.provider_pid,
+                },
+            });
+        }
+    };
 
-  const scopedListItemKeyClasses = "basis-[33%]";
+    const scopedListItemKeyClasses = "basis-[33%]";
 
-  return (
-    <DialogContent className="w-[70dvw] sm:max-w-fit">
-      <DialogHeader>
-        <DialogTitle>Transfer start dialog</DialogTitle>
-        <DialogDescription>
-          <span>You are about to start the transfer process with the following information.</span>
-          {/* <span>{JSON.stringify(process)}</span> */}
-        </DialogDescription>
-      </DialogHeader>
-      {/* List */}
-      <List className="min-w-full  px-2">
-        <ListItem>
-          <ListItemKey className={scopedListItemKeyClasses}>Provider Participant id:</ListItemKey>
-          <Badge variant={"info"}>{process.provider_pid.slice(9, -1)}</Badge>
-        </ListItem>
-        <ListItem>
-          <ListItemKey className={scopedListItemKeyClasses}>Consumer Participant id:</ListItemKey>
-          <Badge variant={"info"}>{process.consumer_pid.slice(9, -1)}</Badge>
-        </ListItem>
-        <ListItem>
-          {role == "provider" && (
-            <>
-              <ListItemKey className={scopedListItemKeyClasses}>Associated consumer:</ListItemKey>
-              <Badge variant={"info"}>{process.associated_consumer.slice(9, -1)}</Badge>
-            </>
-          )}
-          {role == "consumer" && (
-            <>
-              <ListItemKey className={scopedListItemKeyClasses}>Associated provider:</ListItemKey>
-              <Badge variant={"info"}>{process.associated_provider.slice(9, -1)}</Badge>
-            </>
-          )}
-        </ListItem>
-        <ListItem>
-          <ListItemKey className={scopedListItemKeyClasses}>Current state:</ListItemKey>
-          <Badge variant={"status"} state={process.state}>
-            {process.state}
-          </Badge>
-        </ListItem>
-        {process.state_attribute && (
-          <ListItem>
-            <ListItemKey className={scopedListItemKeyClasses}>State attribute:</ListItemKey>
-            <Badge variant={"status"} state={process.state_attribute}>
-              {process.state_attribute}
-            </Badge>
-          </ListItem>
-        )}
-        <ListItem>
-          <ListItemKey className={scopedListItemKeyClasses}>Created at:</ListItemKey>
-          {dayjs(process.created_at).format("DD/MM/YY HH:mm")}
-        </ListItem>
-        {process.updated_at && (
-          <ListItem>
-            <ListItemKey className={scopedListItemKeyClasses}>Updated at:</ListItemKey>
-            {dayjs(process.updated_at).format("DD/MM/YY HH:mm")}
-          </ListItem>
-        )}
-      </List>
-      {/* / List content */}
-      <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          <DialogFooter className="[&>*]:w-full">
-            <DialogClose asChild>
-              <Button variant="ghost" type="reset">
-                Cancel
-              </Button>
-            </DialogClose>
-            <Button type="submit">Start</Button>
-          </DialogFooter>
-        </form>
-      </Form>
-    </DialogContent>
-  );
+    return (
+        <DialogContent className="w-[70dvw] sm:max-w-fit">
+            <DialogHeader>
+                <DialogTitle>Transfer start dialog</DialogTitle>
+                <DialogDescription>
+                    <span>You are about to start the transfer process with the following information.</span>
+                    {/* <span>{JSON.stringify(process)}</span> */}
+                </DialogDescription>
+            </DialogHeader>
+            {/* List */}
+            <List className="min-w-full  px-2">
+                <ListItem>
+                    <ListItemKey className={scopedListItemKeyClasses}>Provider Participant id:</ListItemKey>
+                    <Badge variant={"info"}>{process.provider_pid.slice(9, -1)}</Badge>
+                </ListItem>
+                <ListItem>
+                    <ListItemKey className={scopedListItemKeyClasses}>Consumer Participant id:</ListItemKey>
+                    <Badge variant={"info"}>{process.consumer_pid.slice(9, -1)}</Badge>
+                </ListItem>
+                <ListItem>
+                    {role == "provider" && (
+                        <>
+                            <ListItemKey className={scopedListItemKeyClasses}>Associated consumer:</ListItemKey>
+                            <Badge variant={"info"}>{process.associated_consumer?.slice(9, 40) + "[...]"}</Badge>
+                        </>
+                    )}
+                    {role == "consumer" && (
+                        <>
+                            <ListItemKey className={scopedListItemKeyClasses}>Associated provider:</ListItemKey>
+                            <Badge variant={"info"}>{process.associated_provider?.slice(9, 40) + "[...]"}</Badge>
+                        </>
+                    )}
+                </ListItem>
+                <ListItem>
+                    <ListItemKey className={scopedListItemKeyClasses}>Current state:</ListItemKey>
+                    <Badge variant={"status"} state={process.state}>
+                        {process.state}
+                    </Badge>
+                </ListItem>
+                {process.state_attribute && (
+                    <ListItem>
+                        <ListItemKey className={scopedListItemKeyClasses}>State attribute:</ListItemKey>
+                        <Badge variant={"status"} state={process.state_attribute}>
+                            {process.state_attribute}
+                        </Badge>
+                    </ListItem>
+                )}
+                <ListItem>
+                    <ListItemKey className={scopedListItemKeyClasses}>Created at:</ListItemKey>
+                    {dayjs(process.created_at).format("DD/MM/YY HH:mm")}
+                </ListItem>
+                {process.updated_at && (
+                    <ListItem>
+                        <ListItemKey className={scopedListItemKeyClasses}>Updated at:</ListItemKey>
+                        {dayjs(process.updated_at).format("DD/MM/YY HH:mm")}
+                    </ListItem>
+                )}
+            </List>
+            {/* / List content */}
+            <Form {...form}>
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                    <DialogFooter className="[&>*]:w-full">
+                        <DialogClose asChild>
+                            <Button variant="ghost" type="reset">
+                                Cancel
+                            </Button>
+                        </DialogClose>
+                        <Button type="submit">Start</Button>
+                    </DialogFooter>
+                </form>
+            </Form>
+        </DialogContent>
+    );
 };
--- gui/shared/src/components/ui/badge.tsx
+++ gui/shared/src/components/ui/badge.tsx
@@ -1,97 +1,99 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import {Slot} from "@radix-ui/react-slot";
+import {cva, type VariantProps} from "class-variance-authority";
 
 // @ts-ignore
-import { cn } from "@/lib/utils";
+import {cn} from "@/lib/utils";
 // @ts-ignore
-
-import { DragHandleVerticalIcon } from "@radix-ui/react-icons";
+import {DragHandleVerticalIcon} from "@radix-ui/react-icons";
 
 const badgeVariants = cva(
-  "px-2 py-0.5 w-fit  inline-flex justify-start items-center bg-white/10 font-semibold rounded-sm border whitespace-nowrap shrink-0 gap-1 focus-visible:ring focus-visible:ring-ring/50 focus-visible:ring-[3px] transition-all ",
-  {
-    variants: {
-      variant: {
-        default: "bg-brand-snow/15 text-brand-snow border-transparent",
-        info: "font-mono uppercase bg-background-800 text-secondary-400 border-transparent",
-        infoLighter: "font-mono uppercase bg-white/10 text-secondary-400 border-transparent",
-        role: "text-white uppercase border-transparent",
-        status: "bg-opacity-30 border-transparent text-foreground-300", // base status style
-        // constraint: "bg-white/10 border-0 border-white/15 text-white/60 font-medium break-all"
-      },
-      state: {
-        /* 
-          Importante que los nombres de estas variantes estén en mayúsculas porque es como llega 
-          el string del atributo y si no coincide el casing se rompe 
-          // para poner en state={process.state} y que pille el nombre directamente
-        */
-        default: "",
-        danger: "",
-        warn: "",
-        ACTIVE: "bg-process text-process-300 [&>span]:bg-process-400", // Agreements
-        INACTIVE: "bg-paused text-paused-300 [&>span]:bg-paused-400", // Agreements
-        ACCEPTED: "bg-process text-process-300 [&>span]:bg-process-400", //
-        VERIFIED: "bg-process text-process-300 [&>span]:bg-process-400", // CN
-        STARTED: "bg-process text-process-300 [&>span]:bg-process-400", // Transfers
-        OFFERED: "bg-warn text-warn-300 [&>span]:bg-warn-400", // CN
-        REQUESTED: "bg-warn text-warn-300 [&>span]:bg-warn-400", // CN
-        AGREED: "bg-process text-process-300 [&>span]:bg-process-400", //
-        FINALIZED: "bg-success text-success-300 [&>span]:bg-success-400", // CN
-        COMPLETED: "bg-success text-success-300 [&>span]:bg-success-400", // Transfers
-        SUSPENDED: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
-        PAUSE: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
-        STOP: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
-        TERMINATED: "bg-danger text-danger-300 [&>span]:bg-danger-400", //
-      },
-      role: {
-        Provider: "bg-roles-provider/30",
-        Consumer: "bg-roles-consumer/30",
-        Business: "bg-roles-business/30",
-        Customer: "bg-roles-customer/30",
-      },
-      size: {
-        default: "text-sm px-2 py-0.5",
-        lg: "text-lg font-bold px-2 py-0",
-        // cambie paddings, sino eran muy grandes -Clara
-        // y le meti bg white sino no se veia nada,
-        // estas badges estan en titulos, no sobre el fondo
-        //blanco de la tabla, creo que mejor asi.
-        sm: "text-xs px-1 py-0.5",
-      },
+    "px-2 py-0.5 w-fit  inline-flex justify-start items-center bg-white/10 font-semibold rounded-sm border whitespace-nowrap shrink-0 gap-1 focus-visible:ring focus-visible:ring-ring/50 focus-visible:ring-[3px] transition-all ",
+    {
+        variants: {
+            variant: {
+                default: "bg-brand-snow/15 text-brand-snow border-transparent",
+                info: "font-mono uppercase bg-background-800 text-secondary-400 border-transparent",
+                infoLighter: "font-mono uppercase bg-white/10 text-secondary-400 border-transparent",
+                role: "text-white uppercase border-transparent",
+                status: "bg-opacity-30 border-transparent text-foreground-300", // base status style
+                // constraint: "bg-white/10 border-0 border-white/15 text-white/60 font-medium break-all"
+            },
+            state: {
+                /*
+                  Importante que los nombres de estas variantes estén en mayúsculas porque es como llega
+                  el string del atributo y si no coincide el casing se rompe
+                  // para poner en state={process.state} y que pille el nombre directamente
+                */
+                default: "",
+                danger: "",
+                warn: "",
+                ACTIVE: "bg-process text-process-300 [&>span]:bg-process-400", // Agreements
+                INACTIVE: "bg-paused text-paused-300 [&>span]:bg-paused-400", // Agreements
+                ACCEPTED: "bg-process text-process-300 [&>span]:bg-process-400", //
+                VERIFIED: "bg-process text-process-300 [&>span]:bg-process-400", // CN
+                STARTED: "bg-process text-process-300 [&>span]:bg-process-400", // Transfers
+                OFFERED: "bg-warn text-warn-300 [&>span]:bg-warn-400", // CN
+                REQUESTED: "bg-warn text-warn-300 [&>span]:bg-warn-400", // CN
+                AGREED: "bg-process text-process-300 [&>span]:bg-process-400", //
+                FINALIZED: "bg-success text-success-300 [&>span]:bg-success-400", // CN
+                COMPLETED: "bg-success text-success-300 [&>span]:bg-success-400", // Transfers
+                SUSPENDED: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                PAUSE: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                BY_PROVIDER: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                BY_CONSUMER: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                ON_REQUEST: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                STOP: "bg-pause text-pause-300 [&>span]:bg-pause-400", //
+                TERMINATED: "bg-danger text-danger-300 [&>span]:bg-danger-400", //
+            },
+            role: {
+                Provider: "bg-roles-provider/30",
+                Consumer: "bg-roles-consumer/30",
+                Business: "bg-roles-business/30",
+                Customer: "bg-roles-customer/30",
+            },
+            size: {
+                default: "text-sm px-2 py-0.5",
+                lg: "text-lg font-bold px-2 py-0",
+                // cambie paddings, sino eran muy grandes -Clara
+                // y le meti bg white sino no se veia nada,
+                // estas badges estan en titulos, no sobre el fondo
+                //blanco de la tabla, creo que mejor asi.
+                sm: "text-xs px-1 py-0.5",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            state: "default",
+            size: "default",
+        },
     },
-    defaultVariants: {
-      variant: "default",
-      state: "default",
-      size: "default",
-    },
-  },
 );
 
 function Badge({
-  className,
-  variant,
-  state,
-  size,
-  role,
-  asChild = false,
-  children,
-  ...props
-}: React.ComponentProps<"span"> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot : "span";
-  const showDot = variant === "status";
+                   className,
+                   variant,
+                   state,
+                   size,
+                   role,
+                   asChild = false,
+                   children,
+                   ...props
+               }: React.ComponentProps<"span"> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+    const Comp = asChild ? Slot : "span";
+    const showDot = variant === "status";
 
-  return (
-    <Comp
-      data-slot="badge"
-      className={cn(badgeVariants({ variant, size, state, role }), className)}
-      {...props}
-    >
-      {/* Mete un circulito si la variable es de tipo status y le asigna el color asignado al estado correspondiente */}
-      {showDot && <span className={cn("w-2 h-2 rounded-full mr-1 mb-[2px]")} />}
-      {children}
-    </Comp>
-  );
+    return (
+        <Comp
+            data-slot="badge"
+            className={cn(badgeVariants({variant, size, state, role}), className)}
+            {...props}
+        >
+            {/* Mete un circulito si la variable es de tipo status y le asigna el color asignado al estado correspondiente */}
+            {showDot && <span className={cn("w-2 h-2 rounded-full mr-1 mb-[2px]")}/>}
+            {children}
+        </Comp>
+    );
 }
 
-export { Badge, badgeVariants };
+export {Badge, badgeVariants};
